98043d0319f772cb7c32b6fc796abac42c64f80a
adi1814-ai <aditya.dhanrajdwell10415@gmail.com>
2026-08-31 12:50:50 +0530

    Time: 5 ms (40.73%) | Memory: 106.4 MB (31.80%) - LeetSync

---
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[] nodesBetweenCriticalPoints(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return new int[]{-1, -1};
+        }
+        ListNode prev = head;
+        ListNode curr = head.next;
+        int index = 1;
+
+       
+        int firstCriticalIndex = -1;
+        int prevCriticalIndex = -1;
+        int minDistance = Integer.MAX_VALUE;
+        while (curr.next != null) {
+            
+            boolean isLocalMaxima = (curr.val > prev.val) && (curr.val > curr.next.val);
+            boolean isLocalMinima = (curr.val < prev.val) && (curr.val < curr.next.val);
+
+            if (isLocalMaxima || isLocalMinima) {
+                if (firstCriticalIndex == -1) {
+                    firstCriticalIndex = index;
+                } else {
+                    
+                    minDistance = Math.min(minDistance, index - prevCriticalIndex);
+                }
+              
+                prevCriticalIndex = index;
+            }
+            prev = curr;
+            curr = curr.next;
+            index++;
+        }
+
+        // Check if at least 2 critical points were discovered
+        if (firstCriticalIndex == prevCriticalIndex) {
+            return new int[]{-1, -1};
+        }
+
+        
+        int maxDistance = prevCriticalIndex - firstCriticalIndex;
+        return new int[]{minDistance, maxDistance};
+    }
+}
+    
